87eabdf9e35922c5ec58e2455a3afe9e89c41bc1
litunkhan <litunkhan6@gmail.com>
2023-08-12 20:24:48 +0600

    added-top-rated-and-popular-movie-section

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -70,7 +70,9 @@ const Carousel = ({ data, loading, endpoint, title }) => {
                     {data?.map(item=>{
                         const posterUrl = item.poster_path? url.poster+item.poster_path:PosterFallback
                         return (
-                            <div key={item.id} className="carouselItem" >
+                            <div key={item.id} className="carouselItem" onClick={()=>{
+                                navigate(`/${item.media_type || endpoint}/${item.id}`)
+                            }} >
                                 <div className="posterBlock">
                                     <Img src={posterUrl}></Img>
                                     <CircleRating  rating={item.vote_average.toFixed(1)}/>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,8 @@
 import useTitle from '../../hooks/useTitle';
 import HeroBanner from './heroBanner/HeroBanner';
+import Popular from './popular/Popular';
 import './style.scss'
+import TopRated from './toprated/TopRated';
 import Trending from './trending/Trending';
 
 const Home = () => {
@@ -9,7 +11,9 @@ const Home = () => {
         <div className='homepage'>
            <HeroBanner></HeroBanner> 
            <Trending></Trending>
-           {/* <div style={{height:'1000px'}}></div> */}
+           <Popular></Popular>
+           <TopRated></TopRated>
+           
         </div>
     );
 };
