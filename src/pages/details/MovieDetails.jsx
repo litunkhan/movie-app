e9c6afdc9d0aa9fdf3f4ac472e6c4159e49190cf
litunkhan <litunkhan6@gmail.com>
2023-08-12 23:15:05 +0600

    added-movie-detail

--- src/pages/details/MovieDetails.jsx
+++ src/pages/details/MovieDetails.jsx
@@ -1,11 +1,25 @@
+/* eslint-disable no-unused-vars */
+
+
 
 import './style.scss'
+import DetailsBanner from './detailsBanner/DetailsBanner';
+import useTitle from '../../hooks/useTitle';
+import { useParams } from 'react-router-dom';
+import useFetch from '../../hooks/useFetch';
 
 
 const MovieDetails = () => {
+    useTitle('Details')
+    const { mediaType, id } = useParams();
+    
+    const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+    const { data: credits, loading: creditsLoading } = useFetch(
+        `/${mediaType}/${id}/credits`
+    );
     return (
         <div>
-            
+           <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
         </div>
     );
 };
