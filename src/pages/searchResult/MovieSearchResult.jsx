5649ce8b1bb2ae8c34e55351b128387b210d9aa9
litunkhan <litunkhan6@gmail.com>
2023-08-14 01:56:15 +0600

    added-serach-movie-functionality

--- src/pages/searchResult/MovieSearchResult.jsx
+++ src/pages/searchResult/MovieSearchResult.jsx
@@ -1,13 +1,116 @@
-import './style.scss'
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-unsafe-optional-chaining */
 
 
+
+
+
+
+import  { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+import "./style.scss";
+
+import { fetchDataFromApi } from "../../utils/api";
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+import MovieCard from "../../components/movieCard/MovieCard";
+
+import noResults from "../../assets/._no-results.png";
+import Spinner from "../../components/spiner/Spiner";
 
 const MovieSearchResult = () => {
+    const [data, setData] = useState(null);
+    const [pageNum, setPageNum] = useState(1);
+    const [loading, setLoading] = useState(false);
+    const { query } = useParams();
+
+    const fetchInitialData = () => {
+        setLoading(true);
+        fetchDataFromApi(`/search/multi?query=${query}&page=${pageNum}`).then(
+            (res) => {
+                setData(res);
+                setPageNum((prev) => prev + 1);
+                setLoading(false);
+            }
+        );
+    };
+
+    const fetchNextPageData = () => {
+        fetchDataFromApi(`/search/multi?query=${query}&page=${pageNum}`).then(
+            (res) => {
+                if (data?.results) {
+                    setData({
+                        ...data,
+                        results: [...data?.results, ...res.results],
+                    });
+                } else {
+                    setData(res);
+                }
+                setPageNum((prev) => prev + 1);
+            }
+        );
+    };
+
+    useEffect(() => {
+        setPageNum(1);
+        fetchInitialData();
+    }, [query]);
+
     return (
-        <div>
-            
+        <div className="searchResultsPage">
+            {loading && <Spinner initial={true} />}
+            {!loading && (
+                <ContentWrapper>
+                    {data?.results?.length > 0 ? (
+                        <>
+                            <div className="pageTitle">
+                                {`Search ${
+                                    data?.total_results > 1
+                                        ? "results"
+                                        : "result"
+                                } of '${query}'`}
+                            </div>
+                            <InfiniteScroll
+                                className="content"
+                                dataLength={data?.results?.length || []}
+                                next={fetchNextPageData}
+                                hasMore={pageNum <= data?.total_pages}
+                                loader={<Spinner />}
+                            >
+                                {data?.results.map((item, index) => {
+                                    if (item.media_type === "person") return;
+                                    return (
+                                        <MovieCard
+                                            key={index}
+                                            data={item}
+                                            fromSearch={true}
+                                        />
+                                    );
+                                })}
+                            </InfiniteScroll>
+                        </>
+                    ) : (
+                        
+                           <>
+                            <div style={{width:'100%' ,display:'flex',justifyContent:'center',}}>
+                                <img style={{width:'50%',margin:'auto auto'}} src={noResults} alt="no-result" />
+                               
+
+                            </div>
+                            <div style={{width:'100%' ,display:'flex',justifyContent:'center',}}>
+                            <span className="resultNotFound">
+                            No_Results_Found
+                           </span>
+                           </div>
+                           </> 
+
+                        
+                        
+                    )}
+                </ContentWrapper>
+            )}
         </div>
     );
 };
-
-export default MovieSearchResult;
+export default MovieSearchResult
